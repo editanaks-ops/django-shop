077c2e97bd4c6e2168d9766979d0b6add0762626
Marusja <edita.naks@gmail.com>
2025-12-26 13:44:58 +0100

    Initial commit: Django shop project

---
+++ store/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    image = models.ImageField(upload_to="products/", blank=True, null=True, verbose_name="Изображение")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.name
+
+
+
---
+++ store/views.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+
+def product_list(request):
+    products = Product.objects.all().order_by('-created_at')
+    return render(request, 'store/product_list.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+
+
